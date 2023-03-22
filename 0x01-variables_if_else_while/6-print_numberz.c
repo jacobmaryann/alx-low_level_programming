c3c4065ce3ab3bcbd1b3115ef08c9efad7565595
jacobmaryann <maryann89492@gmail.com>
2023-03-22 04:49:38 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+* main - print numbers from 0 to 9
+* Return: always 0 (Success)
+*/
+int main(void)
+{
+int i;
+for (i = 28; i < 38; i++)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
